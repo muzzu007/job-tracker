8d3d7db89ffc5405322955ff41fd610b91f6c4f1
Muzz <jarvismr0@gmail.com>
2026-08-01 16:22:34 +0530

    Fix MongoDB Atlas DNS resolution

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,9 @@ const app = express();
 const IndexRouter = require('./routes/index');
 const AuthRouter = require('./routes/auth');
 require("dotenv").config();
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const mongoose = require("mongoose");
 
